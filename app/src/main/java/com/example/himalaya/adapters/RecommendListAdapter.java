16bc03cdc5a1955f8b1438db7fdc999967f2268f
clarse <1371309485@qq.com>
2021-01-20 18:09:03 +0800

    complete indicator

---
+++ app/src/main/java/com/example/himalaya/adapters/RecommendListAdapter.java
@@ -0,0 +1,80 @@
+package com.example.himalaya.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.himalaya.R;
+import com.ximalaya.ting.android.opensdk.model.album.Album;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecommendListAdapter extends RecyclerView.Adapter<RecommendListAdapter.InnerHolder> {
+
+    private List<Album> mData = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public InnerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //这里加载view
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recommend, parent, false);
+        return new InnerHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InnerHolder holder, int position) {
+        //这里设置数据
+        holder.itemView.setTag(position);
+        holder.setData(mData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        //返回要显示的个数
+        if (mData != null) {
+            return mData.size();
+        }
+        return 0;
+    }
+
+    public void setData(List<Album> albumList) {
+        if (mData != null) {
+            mData.clear();
+            mData.addAll(albumList);
+        }
+        //更新UI
+        notifyDataSetChanged();
+    }
+
+    public static class InnerHolder extends RecyclerView.ViewHolder {
+        public InnerHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public void setData(Album album) {
+            //找到各个控件。设置数据
+            ImageView albumCover = itemView.findViewById(R.id.album_cover);
+            TextView albumTitle = itemView.findViewById(R.id.album_title_tv);
+            TextView albumDescription = itemView.findViewById(R.id.album_description_tv);
+            TextView albumPlayCount = itemView.findViewById(R.id.album_play_count);
+            TextView albumContentSize = itemView.findViewById(R.id.album_content_size);
+
+            albumTitle.setText(album.getAlbumTitle());
+            albumDescription.setText(album.getAlbumIntro());
+            albumPlayCount.setText(album.getPlayCount() + "");
+            albumContentSize.setText(album.getIncludeTrackCount() + "");
+
+            Glide.with(itemView.getContext()).load(album.getCoverUrlLarge()).into(albumCover);
+
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/himalaya/fragments/RecommendFragment.java
+++ app/src/main/java/com/example/himalaya/fragments/RecommendFragment.java
@@ -1,18 +1,83 @@
 package com.example.himalaya.fragments;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.himalaya.R;
+import com.example.himalaya.adapters.RecommendListAdapter;
 import com.example.himalaya.base.BaseFragment;
+import com.example.himalaya.utils.Constants;
+import com.ximalaya.ting.android.opensdk.constants.DTransferConstants;
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.album.Album;
+import com.ximalaya.ting.android.opensdk.model.album.GussLikeAlbumList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class RecommendFragment extends BaseFragment {
 
+    private static final String TAG = "RecommendFragment";
+    private View mRootView;
+    private RecyclerView mRecyclerView;
+    private RecommendListAdapter mRecommendListAdapter;
+
     @Override
     protected View onSubViewLoaded(LayoutInflater layoutInflater, ViewGroup container) {
-        View rootView = layoutInflater.inflate(R.layout.fragment_recommend, container,false);
-        return rootView;
+        //view加载完成
+        mRootView = layoutInflater.inflate(R.layout.fragment_recommend, container, false);
+        //1.获取控件
+        mRecyclerView = mRootView.findViewById(R.id.recommend_list);
+        //2.设置布局管理器
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        //3.设置适配器
+        mRecommendListAdapter = new RecommendListAdapter();
+        mRecyclerView.setAdapter(mRecommendListAdapter);
+        //拿数据回来
+        getRecommendData();
+
+        //返回view给界面显示
+        return mRootView;
+    }
+
+    /*
+     * 获取推荐内容，猜你喜欢
+     * */
+    private void getRecommendData() {
+        //封装参数
+        Map<String, String> map = new HashMap<>();
+        map.put(DTransferConstants.LIKE_COUNT, Constants.RECOMMEND_COUNT + "");
+        CommonRequest.getGuessLikeAlbum(map, new IDataCallBack<GussLikeAlbumList>() {
+            @Override
+            public void onSuccess(GussLikeAlbumList gussLikeAlbumList) {
+                if (gussLikeAlbumList != null) {
+                    List<Album> albumList = gussLikeAlbumList.getAlbumList();
+                    if (albumList != null) {
+                        upRecommendUI(albumList);
+                    }
+                }
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                Log.d(TAG, "error ---->" + i);
+                Log.d(TAG, "errorMsg ---->" + s);
+            }
+        });
+    }
+
+    private void upRecommendUI(List<Album> albumList) {
+        //把数据给适配器，并更新UI
+        mRecommendListAdapter.setData(albumList);
     }
 
 }
